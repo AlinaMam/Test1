97509b4ed0db3d33c03e4865275301446dcee4ac
Alina Mamontova <alinaamamontova@gmail.com>
2023-04-13 12:35:33 +0300

    new_branch commit

--- src/NewClass.java
+++ src/NewClass.java
@@ -7,5 +7,6 @@ public class NewClass {
         System.out.println("Hello, Alina!");
         System.out.println("Hello, Alina!");
         System.out.println("Hello, Alina!");
+        System.out.println("new_branch");
     }
 }
